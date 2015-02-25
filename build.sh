172c1efa5ac026398c4b79e7840395b279a746db
rogerz42892 <roger@3playmedia.com>
2015-02-25 17:48:05 -0500

    libpng update, ffplay install.

--- build.sh
+++ build.sh
@@ -91,7 +91,7 @@ if [ $nofetch -eq 0 ] ; then
 	../fetchurl "http://sourceforge.net/projects/libpng/files/zlib/1.2.3/zlib-1.2.3.tar.bz2"
     fi
     # upstream
-    ../fetchurl "http://downloads.sourceforge.net/project/libpng/libpng15/1.5.18/libpng-1.5.18.tar.gz"
+    ../fetchurl "http://downloads.sourceforge.net/project/libpng/libpng15/1.5.21/libpng-1.5.21.tar.gz"
     ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz"
     ../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz"
     ../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
